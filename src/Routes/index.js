d8326630580bff824426084a4e1cc74c3bd5d428
WpDavi <100159858+WpDavi@users.noreply.github.com>
2022-06-03 11:49:40 -0300

    ainda funcionando

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Welcome from '../paginas/inicial';
 import Login from '../paginas/Login';
 import Cadastro from '../paginas/Cadastro'
 
 
+import Home from '../paginas/Login/App/Paginas/Home'
+import New from '../paginas/Login/App/Paginas/New'
+import Notification from '../paginas/Login/App/Paginas/Notificatin'
+import Profile from '../paginas/Login/App/Paginas/Profile'
+import Search from '../paginas/Login/App/Paginas/Search'
+
+
 const Stack = createNativeStackNavigator();
+
 
 export default function Routes(){
     return(
@@ -28,5 +37,6 @@ export default function Routes(){
                 options={{ headerShown: false}}
             />
         </Stack.Navigator>
+       
     )
 }
