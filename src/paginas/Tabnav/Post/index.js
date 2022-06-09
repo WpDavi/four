2850b5f29ac390a983efff3b546c91649c7e4d7a
WpDavi <100159858+WpDavi@users.noreply.github.com>
2022-06-09 18:01:13 -0300

    pagina de post em processo

--- src/paginas/Tabnav/Post/index.js
+++ src/paginas/Tabnav/Post/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import {View, Text, Image, StyleSheet, TouchableOpacity, TextInput, SafeAreaView} from 'react-native'
+import {launchImageLibrary } from 'react-native-image-picker';
 
 
 export default function Post() {
+
+
+    function imagePickerCallback(data){
+        console.log(data);
+    if (data.didCancel) {
+      Alert.alert(
+        "Cancelado",
+        "Envio de Image ou video cancelado",)
+    }
+    if (data.error) {
+        Alert.alert(
+            "Erro",
+            "Algo inesperado aconteceu",)
+    }
+    if (!data.uri){
+      return;
+    }
+    setAvatar(data);
+  }
+
     return(
         <SafeAreaView style={styles.container}>
 
@@ -10,36 +31,50 @@ export default function Post() {
 
             <TouchableOpacity>
 
-                <Image style={{width: 25, height: 25,}}
+                <Image style={{width: 35, height: 25,}}
                 source={require('../../../imagens/back.png')}/>
 
             </TouchableOpacity>
 
             <TouchableOpacity>
-                <Text style={styles.posttxt}>Post</Text>
+                <Text style={styles.posttxt}>Postar</Text>
             </TouchableOpacity>
             </View>
 
             <View style={styles.Linha}></View>
 
+            <View style={{ flexDirection:'row', marginTop: 20, marginHorizontal:20  }}>               
+                                 
+            <TouchableOpacity>
+                <Image style={{ width:50, height:50, }}
+                source={require('../../../imagens/perfil.png')} />
+            </TouchableOpacity>
 
             <TextInput style={styles.textimput}
-            placeholder='Oque tem a dizer'>
-                                
-            
-            </TextInput>
+            placeholder='Oque tem a dizer?'
+            multiline={true}
+            placeholderTextColor={'#6C0F6D'}
+             />    
+            </View>
 
-            <Image style={{width:30, height: 30, position:'absolute', bottom:550, right: 40}}
-            source={require('../../../imagens/cam.png')} />  
+
+            <TouchableOpacity  onPress={()=>{launchImageLibrary({}, imagePickerCallback )} } >
+
+            <Image style={{width:30, height: 30, position:'absolute', bottom:-130, right: 30}}
+            source={require('../../../imagens/cam.png')} />
+                
+            </TouchableOpacity>   
+
+              
+            
+ 
 
 
             
             <View style={{width: 370, alignSelf:'center',  marginTop:170, alignItems:'flex-start'}}>
 
-            <Image style={{width:370, resizeMode:'contain', alignItems:'flex-start', alignSelf:'center',  }}
+            <Image style={{width:370, resizeMode:'contain', alignItems:'flex-start', alignSelf:'center'  }}
             source={require('../../../imagens/wall.png')} /> 
-
-            
 
             </View>   
             
@@ -53,7 +88,9 @@ export default function Post() {
 
 const styles = StyleSheet.create({
     container:{
-        flex:1
+        flex:1,
+       
+        
     },
     
 
@@ -67,17 +104,21 @@ const styles = StyleSheet.create({
     Linha:{
         marginTop: 20,
         borderBottomWidth: 1,
-        borderBottomColor: 'red'
+        borderBottomColor: '#6C0F6D'
 
     },
 
     posttxt:{
         fontWeight:'bold',
-        fontSize:20
+        fontSize:20,
+        color: '#6C0F6D'
+        
     },
 
     textimput:{
-        marginLeft: 60
-      
+        marginLeft: 20
+        
     }
+
+    
 })
