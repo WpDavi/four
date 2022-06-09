cfe232d82d14652c510906a86dee664c4f24a80b
WpDavi <100159858+WpDavi@users.noreply.github.com>
2022-06-09 16:03:27 -0300

    estilizando pagina de post

--- src/paginas/Tabnav/Post/index.js
+++ src/paginas/Tabnav/Post/index.js
@@ -1,11 +1,66 @@
 import React from "react";
-import {View, Text} from 'react-native'
+import {View, Text, Image, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
 
 
 export default function Post() {
     return(
         <View>
-            <Text>Testando</Text>
+
+            <View style={styles.containerheader}>
+
+            <TouchableOpacity>
+
+                <Image style={{width: 25, height: 25,}}
+                source={require('../../../imagens/back.png')}/>
+
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text style={styles.posttxt}>Post</Text>
+            </TouchableOpacity>
+            </View>
+
+            <View style={styles.Linha}></View>
+
+
+            <TextInput style={styles.textimput}
+            placeholder='Oque tem a dizer'>
+                                
+            
+            </TextInput>
+
+            <Image style={{width:25, height: 25,}}
+            source={require('../../../imagens/cam.png')} />               
+
+            
+        
+  
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerheader:{
+        marginHorizontal: 30,
+        marginTop:20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    Linha:{
+        marginTop: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: 'red'
+
+    },
+
+    posttxt:{
+        fontWeight:'bold',
+        fontSize:20
+    },
+
+    textimput:{
+        marginLeft: 60
+      
+    }
+})
